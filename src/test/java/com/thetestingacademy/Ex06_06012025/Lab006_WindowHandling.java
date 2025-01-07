1a31c01b5269b06a975597212782eef2e1bf07e3
HemlataNik <morehemlata36@gmail.com>
2025-01-07 11:59:13 +0530

    Added windowHandle files

---
+++ src/test/java/com/thetestingacademy/Ex06_06012025/Lab006_WindowHandling.java
@@ -0,0 +1,47 @@
+package com.thetestingacademy.Ex06_06012025;
+
+import net.bytebuddy.dynamic.loading.ByteArrayClassLoader;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.Set;
+
+public class Lab006_WindowHandling {
+    @Test
+    public void WindowHandle_Test() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/windows");
+        driver.manage().window().maximize();
+
+        WebElement Text = driver.findElement(By.xpath("//*[@class=\"example\"]/child::h3"));
+        String Text1 = Text.getText();
+        System.out.println(Text1);
+
+        //Store the main/parent window or driver to String
+        String mainWindow = driver.getWindowHandle();
+
+        WebElement link = driver.findElement(By.linkText("Click Here"));
+        link.click();
+
+        Set<String> windowHandles = driver.getWindowHandles();
+
+        for(String handle: windowHandles)
+        {
+            driver.switchTo().window(handle);
+            if (driver.getPageSource().contains("New Window"));
+            {
+                System.out.println("Text 'New Window' was found in new window");
+                break;
+            }
+        }
+        //driver.switchTo().window(mainWindow);
+
+        //driver.quit();
+
+
+
+    }
+}
