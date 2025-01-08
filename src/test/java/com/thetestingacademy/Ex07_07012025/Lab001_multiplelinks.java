b4107a37731ab4625112792c8e09b2add7ec6700
HemlataNik <morehemlata36@gmail.com>
2025-01-08 08:21:37 +0530

    All file added

--- src/test/java/com/thetestingacademy/Ex07_07012025/Lab001_multiplelinks.java
+++ src/test/java/com/thetestingacademy/Ex07_07012025/Lab001_multiplelinks.java
@@ -12,17 +12,17 @@ public class Lab001_multiplelinks {
     @Test
     public void multiplelinks_Test() throws InterruptedException {
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.flipkart.com/");
+        driver.get("https://www.w3schools.com/");
         driver.manage().window().maximize();
         Thread.sleep(2000);
 
-       List<WebElement> product_link = (List<WebElement>) driver.findElements(By.xpath("//div[@class=\"_3sdu8W emupdz\"]/a"));
+       List<WebElement> product_link = (List<WebElement>) driver.findElements(By.xpath("//a[text()='Try it Yourself']"));
        int size = product_link.size();
        System.out.println(size);
-//       for(int i= 0; i< size; i++)
-//       {
-//           product_link.get(i).click();
-//           System.out.println(driver.getCurrentUrl());
-//       }
+     for(int i= 0; i< size; i++)
+     {
+       product_link.get(i).click();
+       System.out.println(driver.getTitle());
+     }
     }
 }
