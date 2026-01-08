450b95323398fc31f22826f2b0b6c629374470ec
HemlataNik <morehemlata36@gmail.com>
2026-01-08 19:04:18 +0530

    modified file Lab004

--- src/test/java/com/thetestingacademy/Ex01_18122024/Lab004.java
+++ src/test/java/com/thetestingacademy/Ex01_18122024/Lab004.java
@@ -1,11 +1,17 @@
 package com.thetestingacademy.Ex01_18122024;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class Lab004 {
-    public static void main(String[] args) {
-        WebDriver driver = new EdgeDriver();
+    public static void main(String[] args) throws InterruptedException {
+        //WebDriver driver = new EdgeDriver();
+        WebDriver driver = new ChromeDriver();
         driver.get("https://sdet.live");
+        driver.manage().window().maximize();
     }
 }
